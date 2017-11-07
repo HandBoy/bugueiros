15afa443a5d4f4cdf149ce784bd121b1cf36eec7
handboy <handersonmedeiros@gmail.com>
2017-11-07 11:09:31 -0300

    Added files to login, logout and initing dashboard

--- apps/bugueiro/urls.py
+++ apps/bugueiro/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, dashboard
 
 urlpatterns = [
     url(r'^$', view=index, name='index'),
+    url(r'^dashboard/$', view=dashboard, name='dashboard'),
 ]
--- apps/bugueiro/views.py
+++ apps/bugueiro/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
 def index(request):
     context = {}
     # context['cursos'] = Curso.objects.all()
     return render(request, template_name='index.html', context=context)
+
+
+@login_required
+def dashboard(request):
+    context = {}
+    return render(request, template_name='dashboard.html', context=context)
