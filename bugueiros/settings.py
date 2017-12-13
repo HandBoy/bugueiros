347bc4d9b9f195c7326bc1bd50885d542aeeecad
handboy <handersonmedeiros@gmail.com>
2017-12-13 16:11:29 -0300

    try fix it

--- bugueiros/settings.py
+++ bugueiros/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*hhark!25a6^s)6j)&)%^00^3junm!_nma#)6dmnowudwh1%^#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
